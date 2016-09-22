df42e0d38d71b5e838ea2003643b0714962a834c
Daisy Tucker <daisyatucker@gmail.com>
2016-09-22 12:17:36 +0100

    image added

--- angular/directives/js/controllers/maincontroller.js
+++ angular/directives/js/controllers/maincontroller.js
@@ -1,25 +1,25 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.apps = [ 
     { 
-      icon: 'img/move.jpg', 
+      icon: 'https://hd.unsplash.com/5/unsplash-kitsune-4.jpg', 
       title: 'MOVE', 
       developer: 'MOVE, Inc.', 
       price: 0.99 
     }, 
     { 
-      icon: 'img/shutterbugg.jpg', 
+      icon: 'https://hd.unsplash.com/5/unsplash-kitsune-4.jpg', 
       title: 'Shutterbugg', 
       developer: 'Chico Dusty', 
       price: 2.99 
     },
     {
-      icon: 'img/gameboard.jpg',
+      icon: 'https://hd.unsplash.com/5/unsplash-kitsune-4.jpg',
       title: 'Gameboard',
       developer: 'Armando P.',
       price: 1.99
     },
     {
-      icon: 'img/forecast.jpg',
+      icon: 'https://hd.unsplash.com/5/unsplash-kitsune-4.jpg',
       title: 'Forecast',
       developer: 'Forecast',
       price: 1.99
